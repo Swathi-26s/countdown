1fa3d2af22ff864f5dd86ae7ba1ce2dcf3f5e249
Swathi P <swathiitr113@gmail.com>
2025-05-20 11:19:54 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,20 +1,27 @@
-import {BrowserRouter,Routes,Route } from "react-router-dom"
-import Login from "./pages/Login"
-import Dashboard from "./pages/Dashboard"
-import Register from "./pages/Register"
+import { useState } from "react"
+import Sample from "./components/Sample"
+
 
 function App() {
-  return (
+  const myName = "Swathi"
+  const myAge = 19
+  const myHobby = "Hearing songs"
+  console.log("This is app component", myHobby)
+
+  const [count, setCount] = useState(0)
+  return(
     <>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
-      </Routes>
-      </BrowserRouter>
-    </>
+   <h1 className="text-3xl underline text-violet-600">My React App</h1>
+      <button>Click here</button><br></br>
+      <button className="bg-amber-400 p-2" onClick={() => setCount(count + 1)}>Count {count} </button>
+
+      
+      {/* <Sample myName={myName} myAge={myAge} myHobby={myHobby}/> */}
+      </>
+    
   )
 }
 
+
 export default App
+
